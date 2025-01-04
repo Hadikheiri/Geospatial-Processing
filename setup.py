a635a09948450da0cb4cac489e2304fd2f765ca9
Milad Ramezani Ziarani <milad.ramezani@mail.polimi.it>
2025-01-04 17:30:03 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='atash',
     version='0.1.0',
     authors='Bahman Amirsardary','Hadi Kheiri','Milad Ramezani Ziarani',
-    author_email='bahman.amirsardary@mail.polimi.it','hadi.kheiri@mail.polimi.it','Milad.ramezani@mail.polimi.it',
+    authors_email='bahman.amirsardary@mail.polimi.it','hadi.kheiri@mail.polimi.it','Milad.ramezani@mail.polimi.it',
     description='A library for satellite image processing and fire analysis.',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
